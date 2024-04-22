1fee791d2d08aed7a6743b343a26c906d5a84cbb
Tobi Adeogun <Tobi.Adeogun@stanbicibtc.com>
2024-04-22 14:08:40 +0100

    singly liked list initials

---
+++ 2_Linked_List/Sigle_Linked_List/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList singleLinkedList  = new LinkedList(4);
+
+
+        singleLinkedList.PrintNode();
+
+        System.out.println();
+        singleLinkedList.printHead();
+
+        singleLinkedList.printTail();
+
+        singleLinkedList.printLenght();
+
+    }
+}
